acfbc2b4cd9be18e462c8d64518800a3742659a4
vfalse <2643914281@qq.com>
2024-11-05 18:07:20 +0800

    卡片悬停

--- themes/SimpleBlog/components/Card.js
+++ themes/SimpleBlog/components/Card.js
@@ -8,15 +8,25 @@
 //   </div>
 // }
 // export default Card
+// const Card = ({ children, headerSlot, className }) => {
+//   return (
+//     <div className={`${className} w-12/12`}>
+//       <>{headerSlot}</>
+//       <section className="card shadow-md hover:shadow-md dark:text-gray-300 border dark:border-black rounded lg:p-6 p-4 bg-white dark:bg-hexo-black-gray lg:duration-100">
+//         {children}
+//       </section>
+//     </div>
+//   )
+// }
+// export default Card
+
 const Card = ({ children, headerSlot, className }) => {
   return (
     <div className={`${className} w-12/12`}>
       <>{headerSlot}</>
-      <section className="card shadow-md hover:shadow-md dark:text-gray-300 border dark:border-black rounded lg:p-6 p-4 bg-white dark:bg-hexo-black-gray lg:duration-100">
+      <section className="card shadow-md hover:shadow-xl dark:text-gray-300 border dark:border-black rounded lg:p-6 p-4 bg-white dark:bg-hexo-black-gray transition-all duration-200 ease-in-out hover:scale-[1.02]">
         {children}
       </section>
     </div>
   )
 }
-export default Card
-
